e6b8f9b0e8174cf2197595655e9dc2e6d716f48d
Sam Perkins <perkins.samd12@gmail.com>
2024-05-14 18:23:59 -0500

    chore(packages): tailwind and vercel-adapater

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import '../app.css';
+</script>
+
+<header>The Gumball Club</header>
+<div class="flex gap-4 p-4 w-full">
+	<main class="max-w-[1000px] bg-neutral-800 m-auto w-full text-red-500">
+		<slot />
+	</main>
+</div>
+<footer>footer</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
+<script lang="ts">
+	let date: Date;
+
+	function onDateChange(event: Event) {
+		const target = event.target as HTMLInputElement;
+		if (target.value) {
+			date = new Date(target.value);
+		}
+	}
+
+	function isPokerNight(date: Date) {
+		return (
+			date.getDay() === 4 && ((date.getDate() > 7 && date.getDate() < 15) || date.getDate() > 28)
+		);
+	}
+
+	function nextPokerNight() {
+		return 'soon';
+	}
+</script>
+
+<h1 class="text-red-500">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<div class="text-center">
+	<h2>Countdown until next poker night: <span>{nextPokerNight()}</span></h2>
+	<input type="date" class="bg-neutral-900 placeholder-cyan-500" on:change={onDateChange} />
+	<span>date is: {date ? (isPokerNight(date) ? 'POKER NIGHT' : 'lame') : 'select date'}</span>
+</div>
